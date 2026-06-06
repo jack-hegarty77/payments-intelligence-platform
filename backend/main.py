bc5e66500fbcb5586c52493dd42521934f834269
Jack Hegarty <jackhegarty@Mac.Home>
2026-06-06 11:48:54 +0100

    Add gitignore and CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models import Payment
 from risk_engine import calculate_risk
 
 app = FastAPI(title="Payments Intelligence API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
